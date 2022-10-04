33b34b69d2fb19dc960481378e88882786daa4b5
omotega <tomoyibo@gmail.com>
2022-10-04 03:06:34 +0100

    made update to the user controller

--- src/controllers/usercontroller.ts
+++ src/controllers/usercontroller.ts
@@ -5,39 +5,39 @@ import { comparePassword, hashPassword } from '../utils/hash';
 import { generateToken } from '../utils/token';
 
 export const Register = async (req: Request, res: Response) => {
-    try {
-        const { name, email, password } = req.body;
-        if (!name || !email || !password) return errorResponse(res, 400, 'incomplete credentials');
-        const userExists = await User.findOne({ email: email });
-        if (userExists) return errorResponse(res, 400, 'User already exists');
-        const hash = await hashPassword(password);
-        const user = await User.create({
-            name,
-            email,
-            password: hash,
-        });
-        if (!user) return errorResponse(res, 400, 'incorrect password');
-        return successResponse(res, 201, 'account created succesfully', user);
-    } catch (error) {
-        handleError(error, req);
-        return errorResponse(res, 500, 'server error');
-    }
+  try {
+    const { name, email, password} = req.body;
+    if (!name || !email || !password) return errorResponse(res, 400, 'incomplete credentials');
+    const userExists = await User.findOne({ email: email });
+    if (userExists) return errorResponse(res, 400, 'User already exists');
+    const ispassword = await hashPassword(password);
+    if (!ispassword) return errorResponse(res, 400, 'incorrect password');
+    const user = await User.create({
+      name,
+      email,
+      password: ispassword,
+    });
+    return successResponse(res, 201, 'account created succesfully', user);
+  } catch (error) {
+    handleError(error, req);
+    return errorResponse(res, 500, 'server error');
+  }
 }
 
 export const login = async (req: Request, res: Response) => {
-    try {
-        const { email,password } = req.body;
-        if(!email || !password) return errorResponse(res,400,'incomplete credentials');
-        const user = await User.findOne({email});
-        if(!user) return errorResponse(res,404,'User does not exists');
-        const validatePassword = await comparePassword(password,user.password);
-        if(!validatePassword) return errorResponse(res,400,'wrong Password')
-        const token = await generateToken({id:user._id,role:user.role});
-        return successResponse(res,200,'user logged in successfully',{user,token});
-        
-    } catch (error) {
-        handleError(error,req);
-        return errorResponse(res, 500, 'server error');
-    }
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) return errorResponse(res, 400, 'incomplete credentials');
+    const user = await User.findOne({ email });
+    if (!user) return errorResponse(res, 404, 'User does not exists');
+    const validatePassword = await comparePassword(password, user.password);
+    if (!validatePassword) return errorResponse(res, 400, 'wrong Password')
+    const token = await generateToken({ _id: user._id, name: user.name, role: user.role });
+    return successResponse(res, 200, 'user logged in successfully', { user, token });
+
+  } catch (error) {
+    handleError(error, req);
+    return errorResponse(res, 500, 'server error');
+  }
 
 }
