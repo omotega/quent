9b29be6f8cbd20abdba74a068fe648b94023daa0
omotega <tomoyibo@gmail.com>
2022-10-04 03:08:02 +0100

    made update to my app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import { CustomRequest } from './utils/interface'
 import  dbConnect  from './config/db';
 import userRouter from './routes/userroutes';
+import adminRouter from './routes/adminroutes';
 
 
 dotenv.config();
@@ -13,11 +15,18 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+declare global {
+    namespace Express {
+        interface Request extends CustomRequest {}
+    }
+}
+
 app.get('/',(req,res) => {
     res.send('welcome to quent');
 })
 
 app.use('/api/v1/users',userRouter);
+app.use('/api/v1/admin',adminRouter);
 
 dbConnect();
 app.listen(port, () => console.log(`port running on port ${port}`));
