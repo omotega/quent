bf4ebbbfc56749c610d7018a958459fc2da3e368
omotega <tomoyibo@gmail.com>
2022-10-04 03:03:58 +0100

    added custom interface to my interface file

--- src/utils/interface.ts
+++ src/utils/interface.ts
@@ -1,6 +1,16 @@
 export interface Iuser {
+    _id?:string
     name:string,
     email:string,
+    address?:string
     password:string,
     role?:string,
 }
+export interface CustomRequest{
+    user:Iuser,
+    file:object,
+    params:object,
+    query:object,
+    path:object,
+}
+
