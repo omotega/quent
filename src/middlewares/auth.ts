0ce43600d0558d0e716ad8efb652ce28ff2832bc
omotega <tomoyibo@gmail.com>
2022-10-04 02:59:26 +0100

    added middleware  authentication for user and admin

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,46 @@
+import { Request, Response,NextFunction } from 'express';
+import { errorResponse, handleError } from '../utils/response';
+import { decodeToken } from '../utils/token';
+import User from '../models/usermodel';
+
+
+export const authProtect = async(req: Request, res: Response,next:NextFunction) => {
+    try {
+        if(req.headers && req.headers.authorization) {
+            const token = req.headers.authorization.split(' ')[1];
+            const decode:any = await decodeToken(token);
+            const user = await User.findById(decode.id);
+            if(!user) return errorResponse(res,401,'user not found');
+            req.user = user;
+           return next()
+        } else {
+            return errorResponse(res,401,'authorization not found');
+        }
+            
+    } catch (error:any) {
+        return errorResponse(res, 500, error.message);
+    }
+}
+
+export const verifyAdmin = async(req: Request, res: Response,next:NextFunction) => {
+    try {
+        const { _id } = req.user;
+        const admin = await User.findOne({_id,role:'admin'});
+        if(!admin) return errorResponse(res,401,'unauthorized access');
+        return next()
+    } catch (error:any) {
+        return errorResponse(res, 500, error.message);
+    }
+}
+
+export const verifyLandlord = async(req: Request, res: Response,next: NextFunction) => {
+    try {
+        const { _id } = req.user;
+        const landlord = await User.findOne({_id,role:'landlord'});
+        if(!landlord) return errorResponse(res,401,'unathorized access');
+        return next()
+    } catch (error:any) {
+        return errorResponse(res, 500, error.message);
+    }
+}
+
