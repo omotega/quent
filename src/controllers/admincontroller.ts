eadbc895f4621f83350f5ae251343cdabab4edae
omotega <tomoyibo@gmail.com>
2022-10-10 03:00:49 +0100

    updated the admincontroller by adding get all users feature

--- src/controllers/admincontroller.ts
+++ src/controllers/admincontroller.ts
@@ -1,21 +1,12 @@
 import { Request, Response } from 'express';
 import { errorResponse, handleError, successResponse } from '../utils/response';
 import User from '../models/usermodel';
-import { hashPassword } from '../utils/hash';
 
 
-export const registerLandlord = async (req: Request, res: Response) => {
+export const getAllusers = async (req: Request, res:Response) => {
   try {
-    const { name, email, address, password,role } = req.body;
-    if (!name || !email || !address || !password || !role) return errorResponse(res, 400, 'incomplete credentials');
-    const landlordExist = await User.findOne({email});
-    if(landlordExist) return errorResponse(res,401, 'landlord already exist');
-    const ispassword = await hashPassword(password);
-    if(!ispassword) return errorResponse(res, 400,'incorrect password');
-    const landlord = await User.create({ 
-      name,email,address,password:ispassword,role,
-    });
-    return successResponse(res,201,'landlord created succesfully',landlord)
+    const user = await User.find({ });
+    return successResponse(res,200,'users found',user);
   } catch (error) {
     handleError(error,req);
     return errorResponse(res, 500, 'server error');
